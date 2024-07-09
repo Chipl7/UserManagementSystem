37f247d21e235a016ce8f33b06d5f015195e146f
Artem <artem.chipl@gmail.com>
2024-07-10 01:05:54 +0700

    udate UsersController

--- src/main/java/controllers/UsersController.java
+++ src/main/java/controllers/UsersController.java
@@ -1,16 +1,28 @@
 package controllers;
 
 import entity.User;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/users")
+@RequestMapping("/users")
 public class UsersController {
 
-    public List<User> findAllUser(){
-        return List.of();
+    @GetMapping
+    public List<User> findAllUsers() {
+        List<User> users = new ArrayList<>();
+        User user = new User();
+        user.setFirstName("Андрей");
+        user.setLastName("Казанцев");
+        user.setDateOfBirth(LocalDate.of(2000, 10, 18));
+        user.setEmail("Pepa@inbox.ru");
+        user.setAge(23);
+        users.add(user);
+        return List.of(user);
     }
 }
